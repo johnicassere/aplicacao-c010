73a0c37bd9b1ff929d5c2f7c614ba53056608173
Johni Cassere <johnicassere@hotmail.com>
2022-02-11 01:01:09 -0300

    Front

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { NavBar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Main } from './pages/Main';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { Mesa } from './components/Mesa';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <NavBar/>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Main/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/cadastrar" element={<Register/>} />
+            <Route path="/mesa" element={<Mesa/>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
